3361610ab0b63ce4933d77c4bce103f39677b5a1
Thiago Giammattey <giammattey@gmail.com>
2018-12-26 08:18:20 -0200

    model de usuário e autoload

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const app = require('express')();
 const mongoose = require('mongoose');
+const requireDir = require('require-dir');
 
 const dbConfig = require('./config/database');
 
 mongoose.connect(dbConfig.url);
+requireDir(dbConfig.modelsPath);
+
+const User = mongoose.model('User');
+User.create(
+  {
+    name: 'Diego',
+    username: 'diego3g',
+    email: 'diego@rocketseat.com.br',
+    password: '123456',
+  },
+  () => {
+    console.log('Ok!');
+  },
+);
 
 app.listen(3000);
